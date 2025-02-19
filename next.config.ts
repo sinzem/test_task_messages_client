57510842dd609ae1eb991aadfcb356c9a738162e
Sergey <sinzem@gmail.com>
2025-02-20 00:28:21 +0200

    fix_config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: `${process.env.NEXT_PUBLIC_APP_SERVER_URL}/:path*`, 
-  //     },
-  //   ]
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_APP_SERVER_URL}/:path*`, 
+      },
+    ]
+  },
   experimental: {
     serverActions: {
       allowedOrigins: ["sinzem.uno", "localhost:3000"]
